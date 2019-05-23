903fc18696d24c43ce08b57b3d27208e69b2ff28
lawrence <lawrence.t.yen@gmail.com>
2019-05-22 22:59:20 -0700

    user controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,8 +4,28 @@ module.exports = {
   create(req, res, next) {
     let newUser = {
       email: req.body.email,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
       password: req.body.password,
       password_conf: req.body.password_conf
     }
+    userQueries.createUser(newUser, (err, user) => {
+      if(err) {
+        console.log(err);
+        let returnData = {
+          statusCode: 400,
+          message: 'Bad Request',
+          data: err
+        };
+        res.json(returnData)
+      } else {
+        let returnData = {
+          statusCode: 200,
+          message: 'Success',
+          data: user
+        };
+        res.json(returnData)
+      }
+    })
   }
 }
